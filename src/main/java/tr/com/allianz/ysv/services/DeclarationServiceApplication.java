e499ccb6a9f07bad5101c5d12ac5e67d49d4e33e
Muhammed <50794266+m-ogur34@users.noreply.github.com>
2026-09-22 01:38:21 +0300

    Update DeclarationServiceApplication.java

--- src/main/java/tr/com/allianz/ysv/services/DeclarationServiceApplication.java
+++ src/main/java/tr/com/allianz/ysv/services/DeclarationServiceApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-/**
- * Entry point of the SBM YSV declaration transfer service.
- */
+
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class DeclarationServiceApplication {
